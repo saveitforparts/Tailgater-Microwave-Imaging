4dbfe9ada09ed59c6cd490507f3d8f276c4215f8
saveitforparts <84099241+saveitforparts@users.noreply.github.com>
2023-05-02 16:40:39 -0500

    Add files via upload

--- dish_scan.py
+++ dish_scan.py
@@ -1,4 +1,5 @@
 #Python program to scan with Dish Tailgater and output signal strength bitmap
+#Version 2.0
 #Gabe Emerson / Saveitforparts 2023, Email: gabe@saveitforparts.com
 
 import serial
@@ -56,14 +57,12 @@ if el_end > 70:
 	print('Elevation out of range, setting to 70')
 	el_end=70
 
-#########This method doesn't work reliably, "nudge" results in too much motor drift on azimuth axis
-#Choose between between azangle/elangle for low res and aznudge/elnudge for high res
-#resolution = int(input('Resolution (1=low, 2=high, default 1): ') or 1)
-#if not resolution in (1,2):
-#	print('Unknown selection, setting to 1 (low)')
-#	resolution=1 
 
-resolution = 1 #hard-coding this until if/when we figure out reliable motor steps smaller than full degrees
+#Choose between between azangle/elangle for low res and aznudge/elnudge for high res
+resolution = int(input('Resolution (1=low, 2=high, default 1): ') or 1)
+if not resolution in (1,2):
+	print('Unknown selection, setting to 1 (low)')
+	resolution=1 
 
 
 np.savetxt("scan-settings-" + timestr +".txt", (az_start,az_end,el_start,el_end,resolution))	
@@ -74,8 +73,19 @@ if resolution==1: #standard scan using angles
 
 	#Provide runtime estimate (I'm making a rough guess based on prior runs on two different computers)
 	time_est = az_range * el_range
-	print ('Estimated scan time with your parameters is ', (time_est+(time_est/6))/60, ' minutes.')
+	time_output = (time_est+(time_est/6))/60
+	time_output = round(time_output, 2)
+	if time_output > 60:
+		print ('Estimated scan time with your parameters is ', time_output/60, ' hours.')
+	else:
+		print ('Estimated scan time with your parameters is ', time_output, ' minutes.')
 	print ('')
+	user_confirm = input('Proceed with scan? (y/n):')
+	if user_confirm.lower().startswith("y"):
+		print ('Scan in progress...')
+	else:
+		print ('exiting.')
+		exit()
 
 	#create bitmap to preview signal strengths
 	sky_image = Image.new('RGB', [az_range+1,el_range+1], 255) 
@@ -84,7 +94,7 @@ if resolution==1: #standard scan using angles
 	#create 2D array for raw signal strengths
 	sky_data = np.zeros((el_range+1,az_range+1))
 
-	print ("Moving dish to staring position...")
+	print ("Moving dish to starting position...")
 	#initialize starting dish position
 	#for some reason, the dish only receives single char from each write(), so send one at a time
 	#(wow much kludge, such ugly)
@@ -109,7 +119,7 @@ if resolution==1: #standard scan using angles
 	        dish.write(angle.encode())
 	dish.write(b'\r')
 
-	time.sleep(5)   #Give motors time to drive dish to scan origin
+	time.sleep(10)   #Give motors time to drive dish to scan origin
 	dish.flush()
 	dish.reset_output_buffer()
 
@@ -136,7 +146,7 @@ if resolution==1: #standard scan using angles
 	dish.flush()
 	dish.reset_output_buffer()
 	dish.reset_input_buffer()
-	time.sleep(5)
+	time.sleep(10)
 		
 	direction=0		
 	#aiming loop
@@ -146,8 +156,10 @@ if resolution==1: #standard scan using angles
 		#valid azangle 0-360. Remember dish increments ccw. 
 		for azimuth in range (az_start,az_end):
 	
-			if (direction % 2) == 0: 		  #check for sweep direction
-				azimuth = abs(azimuth-az_end)+az_start  	  #increment backwards on even numbered loops
+			#Eliminated alternate scan directions in version 2.0. It was slightly faster,
+			#But caused too many indexing and gear meshing issues. 
+			#if (direction % 2) == 0:   #check for sweep direction
+			#	azimuth = abs(azimuth-az_end)+az_start   #increment backwards on even numbered loops
 				
 			print("Azimuth: ", azimuth, ", Elevation: ", elevation)  #display current position
 
@@ -225,6 +237,62 @@ if resolution==1: #standard scan using angles
 			sky_image.save('result-' + timestr +'.png')
 
 
+
+		#Move dish slightly farther to allow gear meshing 	
+		#if (direction % 2) == 0:
+		#	angle = str(int(angle) - 2)
+		#else:
+		#	angle = str(int(angle) + 2)
+		#dish.write(b'a')
+		#dish.write(b'z')
+		#dish.write(b'a')
+		#dish.write(b'n')
+		#dish.write(b'g')
+		#dish.write(b'l')
+		#dish.write(b'e')
+		#dish.write(b' ')
+		#we have to break up 3 and 2 digit numbers into single characters
+		#if (len(angle)==3):
+		#        dish.write(angle[0].encode())
+		#        dish.write(angle[1].encode())
+		#        dish.write(angle[2].encode())
+		#elif len(angle)==2:
+		#        dish.write(angle[0].encode())
+		#        dish.write(angle[1].encode())
+		#else: #shouldn't need this for typical range, but might want it later
+		        #single-digit number
+		#        dish.write(angle.encode())
+		#dish.write(b'\r')
+		#time.sleep(2)
+		
+		
+		
+		#Return to starting azimuth for each elevation position
+		dish.write(b'a')
+		dish.write(b'z')
+		dish.write(b'a')
+		dish.write(b'n')
+		dish.write(b'g')
+		dish.write(b'l')
+		dish.write(b'e')
+		dish.write(b' ')
+		angle=str(az_start)
+		if (len(angle)==3):
+			dish.write(angle[0].encode())
+			dish.write(angle[1].encode())
+			dish.write(angle[2].encode())
+		elif len(angle)==2:
+			dish.write(angle[0].encode())
+			dish.write(angle[1].encode())
+		else: 
+		        #single-digit number
+		        dish.write(angle.encode())
+		dish.write(b'\r')		
+		
+		#delay to allow dish to return to starting azimuth, dynamic based on az_range
+		wait_time = int((az_range)*0.05)+1 #Dish takes ~0.05sec to rotate 1 degree, plus 1 sec buffer
+		time.sleep(wait_time)
+		
 		#command dish to next elevation
 		dish.write(b'e')
 		dish.write(b'l')
@@ -244,17 +312,30 @@ if resolution==1: #standard scan using angles
 		        dish.write(angle.encode())
 		dish.write(b'\r')
 
-#####################################Currently resolution 2 is not enabled, because "nudge" isn't consistent
+
 elif resolution==2: #high res scan using nudge instead of angle
 
-	#"nudge" is "about 0.2 degrees", so for each degree in range we have 5 dish positions
+	#"nudge" is "about 0.2 degrees" azimuth and about 0.33 degrees elevation
+	#So for each degree in Az range we have 5 dish positions, and for each degree in El we have 3 positions. 
 	az_range = (az_end - az_start)*5
-	el_range = (el_end - el_start)*5
+	el_range = (el_end - el_start)*3
 
 	#Provide runtime estimate (I'm making a rough guess based on prior runs on two different computers)
 	time_est = az_range * el_range
-	print ('Estimated scan time with your parameters is ', (time_est+(time_est/6))/60, ' minutes.')
+	time_output = ((time_est+(time_est/6))/60)+((el_range*10)/60) #High-res takes longer, returns to az_start on each elevation
+	time_output = round(time_output, 2)
+	if time_output > 60:
+		print ('Estimated scan time with your parameters is ', time_output/60, ' hours.')
+	else:
+		print ('Estimated scan time with your parameters is ', time_output, ' minutes.')
 	print ('')
+	user_confirm = input('Proceed with scan? (y/n):')
+	if user_confirm.lower().startswith("y"):
+		print ('Scan in progress...')
+	else:
+		print ('exiting.')
+		exit()
+		
 
 	#create bitmap to preview signal strengths
 	sky_image = Image.new('RGB', [az_range+1,el_range+1], 255) 
@@ -263,7 +344,7 @@ elif resolution==2: #high res scan using nudge instead of angle
 	#create 2D array for raw signal strengths
 	sky_data = np.zeros((el_range+1,az_range+1))
 
-	print ("Moving dish to staring position...")
+	print ("Moving dish to starting position...")
 	#initialize starting dish position
 	#for some reason, the dish only receives single char from each write(), so send one at a time
 	#(wow much kludge, such ugly)
@@ -288,7 +369,7 @@ elif resolution==2: #high res scan using nudge instead of angle
 	        dish.write(angle.encode())
 	dish.write(b'\r')
 
-	time.sleep(5)   #Give motors time to drive dish to scan origin
+	time.sleep(10)   #Give motors time to drive dish to scan origin
 	dish.flush()
 	dish.reset_output_buffer()
 
@@ -315,19 +396,16 @@ elif resolution==2: #high res scan using nudge instead of angle
 	dish.flush()
 	dish.reset_output_buffer()
 	dish.reset_input_buffer()
-	time.sleep(5)
-	direction=0		
+	time.sleep(10)
 					
 	#aiming loop
 	#valid elangle range appears to be ~2-73 degrees, use slightly smaller range to avoid motor overrun. 
 	for elevation in range (0,el_range):
-		direction = direction+1
 		#valid azangle 0-360. Remember dish increments ccw. 
+		
+		
 		for azimuth in range (0,az_range):
 	
-			if (direction % 2) == 0: 		  #check for sweep direction
-				azimuth = (abs(azimuth-az_range))  	  #increment backwards on even numbered loops
-				
 			print("X: ", azimuth, ", Y: ", elevation)  #display current position
 
 			#command dish to next azimuth	
@@ -340,8 +418,7 @@ elif resolution==2: #high res scan using nudge instead of angle
 			dish.write(b'e')
 			dish.write(b' ')
 			dish.write(b'c')
-			if not (direction % 2) == 0: 		  #check for sweep direction
-				dish.write(b'c')			
+			dish.write(b'c')
 			dish.write(b'w')
 			dish.write(b'\r')
 			
@@ -395,7 +472,32 @@ elif resolution==2: #high res scan using nudge instead of angle
 			#write bitmap to time stamped image file
 			sky_image.save('result-' + timestr +'.png')
 			
-			
+		#Return to starting azimuth for each elevation position
+		dish.write(b'a')
+		dish.write(b'z')
+		dish.write(b'a')
+		dish.write(b'n')
+		dish.write(b'g')
+		dish.write(b'l')
+		dish.write(b'e')
+		dish.write(b' ')
+		angle=str(az_start)
+		if (len(angle)==3):
+			dish.write(angle[0].encode())
+			dish.write(angle[1].encode())
+			dish.write(angle[2].encode())
+		elif len(angle)==2:
+			dish.write(angle[0].encode())
+			dish.write(angle[1].encode())
+		else: 
+		        #single-digit number
+		        dish.write(angle.encode())
+		dish.write(b'\r')		
+		
+		#delay to allow dish to return to starting azimuth, dynamic based on az_range
+		wait_time = int((az_range/5)*0.05)+1 #Dish takes ~0.05sec to rotate 1 degree, plus 1 sec buffer
+		time.sleep(wait_time)
+		
 		#command dish to next elevation
 		dish.write(b'e')
 		dish.write(b'l')
@@ -407,11 +509,13 @@ elif resolution==2: #high res scan using nudge instead of angle
 		dish.write(b' ')
 		dish.write(b'u')
 		dish.write(b'p')
-		dish.write(b'\r')	
-##################################Resolution 2 not working, ignore above elif
+		dish.write(b'\r')
+		
+		
+
 
 else: #possibly implement medium resolution in the future?
-	print ('')
+	print ('Invalid resolution')
 
 print ('Scan complete!')
 #close serial connection
